a835d697dbc1db5402de80cb2e39712a582c21cc
Pho Hale <phohale@email.arizona.edu>
2019-01-08 21:38:29 -0500

    Broke out run phase scripts into external scripts

---
+++ DeployScripts/PostBuildInstall.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  PostBuildInstall.sh
+#  
+#
+#  Created by Pho Hale on 01/08/19.
+#
+
+/Users/pho/Dropbox/Coding/Executables/symboliclinker/InstallScript.command
+
+# “Show environment variables in build log” = TRUE
+
+## Output Files
+$(DERIVED_FILE_DIR)/outputLog.txt
+$(DERIVED_FILE_DIR)/outputLogPBS.txt
